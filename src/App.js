bbaa1e0c5ec01797f56e134146c7383f9d561562
Aashika Jetti <aashikajetti@gmail.com>
2024-05-09 14:47:37 +0530

    1:01:45 seconds in yt video. Created MovieCard.jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from 'react';
 
-function App() {
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL ='http://www.omdbapi.com?apikey=d37fdd62';
+
+const movie1={
+  "Poster":"https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_SX300.jpg",
+  "Title"
+  : 
+  "Captain Marvel",
+  "Type"
+  : 
+  "movie",
+  "Year"
+  : 
+  "2019",
+  "imdbID"
+  : 
+  "tt4154664"}
+
+const App = () => {
+
+  //searchMovies is an arrow fn 
+                  //async is for data => it takes time to fetch data
+  const searchMovies = async(title) =>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data=await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(()=>{
+    searchMovies('Marvel');       
+  },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1> Movies?</h1>
+      <div className='search'>
+        <input
+          placeholder='Search for movies' 
+          value="Superman" 
+          onChange={()=>{}}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={()=>{}}
+        />
+      </div>
+
+      <div className='container'>
+        
+      </div>
     </div>
   );
 }
 
+//exporting so that we can call it from somewhere else
 export default App;
